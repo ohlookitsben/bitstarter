9b46b201cb3fabec975bd511fa5570aa3ef4bbbe
Ben Wiles <ben.william.wiles@gmail.com>
2013-07-08 11:21:19 +0000

    Added read from index file for get '/'

--- web.js
+++ web.js
@@ -2,8 +2,12 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var readIndex = function () {
+    return fs.readFileSync('index.html', 'utf8');
+});
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(readIndex());
 });
 
 var port = process.env.PORT || 5000;
